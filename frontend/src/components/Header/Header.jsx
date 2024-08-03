f394d3e30a382a941ad509f29b584749711cc5b5
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-03 18:45:53 +0300

    Initial

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Deliciousness delivered right to your doorstep! 🍛🚚</h2>
+            <p>Welcome to Food Hub! We’re your go-to destination for mouthwatering meals delivered right to your doorstep. Explore our menu and satisfy your cravings today! Bon appétit!</p>
+            <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
