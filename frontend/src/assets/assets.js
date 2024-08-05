c9b21e0d0a0504f1e3c2fa216fa3d08420a7f39a
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-05 20:23:31 +0300

    Initial

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -59,9 +59,11 @@ import profile_icon from './profile_icon.png'
 import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
+import Frame_1 from './Frame_1.png'
 
 export const assets = {
     logo,
+    Frame_1,
     basket_icon,
     header_img,
     search_icon,
