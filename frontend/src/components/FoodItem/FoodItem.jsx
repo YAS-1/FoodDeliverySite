a02de0b759a2a6705144335ceda436f3110c8735
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-09 12:50:40 +0300

    Initial

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,25 +1,28 @@
 
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./FoodItem.css";
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
 {/*The Food Item that handles the display for each individual food item*/}
-function FoodItem({id, name, price, description, image}) {
+function FoodItem({id, name, price, image}) {
 
-    const [itemCount, setItemCount] = useState(0);
+
+    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
 
   return (
     <div className='food-item'>
         <div className="food-item-image-container">
             <img className='food-item-image' src={image} alt="" />
 
-            {!itemCount ?
-                <img src={assets.add_icon_white} onClick={() => setItemCount(pre =>pre+1)} className="add" />
+            {!cartItems[id] ?
+                <img src={assets.add_icon_white} onClick={() => addToCart(id)} className="add" />
                 :
                 <div className="food-item-counter">
-                    <img src={assets.remove_icon_red} onClick={() => setItemCount(pre=>pre-1)}/>
-                    <p>{itemCount}</p>
-                    <img src={assets.add_icon_green} onClick={() => setItemCount(pre=>pre+1)}/>
+                    <img src={assets.remove_icon_red} onClick={() => removeFromCart(id)}/>
+                    {/*The value of the Itemid displayed*/}
+                    <p>{cartItems[id]}</p>
+                    <img src={assets.add_icon_green} onClick={() => addToCart(id)}/>
                 </div>
             }
 
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,7 +1,7 @@
 
 
 {/*Using context to allow multiple compnents to access values without using props*/}
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
@@ -9,9 +9,32 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
 
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) =>{
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 })); // If the itemId is not in the cart then it will be added and its value will be 1
+        }
+        else{
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1})); // If the itemId already exist in the cart ten its value will be increased by 1
+        }
+    }
+
+    const removeFromCart = (itemId) =>{
+        setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1})) // The itemId value will be decreased by 1
+    }
+
+    useEffect(()=>{
+        console.log(cartItems)
+    },[cartItems])
+
     const contextValue = {
         // Add your context values here
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
 
     return (
