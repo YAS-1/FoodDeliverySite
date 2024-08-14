36085c6cec81c492801ccec70f58731ff3f8c63e
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-14 12:51:23 +0300

    Initial

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -44,6 +44,7 @@ import food_30 from './food_30.png'
 import food_31 from './food_31.png'
 import food_32 from './food_32.png'
 
+
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
 import remove_icon_red from './remove_icon_red.png'
@@ -349,6 +350,5 @@ export const food_list = [
         image: food_32,
         price: 15,
         description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }
+        category: "Noodles"}
 ]
