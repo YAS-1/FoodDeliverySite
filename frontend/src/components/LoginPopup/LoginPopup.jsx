49bc11cb686df74fd411e350fd382e3a45279923
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-18 09:00:45 +0300

    Initial

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -7,11 +7,25 @@ function LoginPopup({setShowLogin}) {
 
   return (
     <div className='login-popup'>
-        <form action="" className="login-pop-container">
-            <div className="login-pop-title">
+        <form action="" className="login-popup-container">
+            <div className="login-popup-title">
                 <h2>{currentState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
+            <div className="login-popup-input">
+              {currentState==="Login"?<></>:<input type="text"placeholder='Name' required/>}
+
+                <input type="email"placeholder='Email' required/>
+                <input type="password"placeholder='Password' required/>  
+            </div>
+            <button>{currentState==="Sign Up" ? "Create Account":"Login"}</button>
+            <div className="login-popup-condition">
+              <input type="checkbox" required />
+              <p>I agree to the terms of use & privacy policy.</p>
+            </div>
+            {currentState==="Login"?
+            <p>Don't have an account? <span onClick={()=>setCurrentState("Sign Up")}>Create an Account</span></p>
+            :<p>Already have an account? <span onClick={()=>setCurrentState("Login")}>Login</span></p>}
         </form>
     </div>
   )
