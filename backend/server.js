c7ad3a9352f4a141cf818298720ecb735eb19072
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-29 09:53:36 +0300

    Initial

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,13 @@ import cors from "cors"
 // The app config
 const app = express();
 const port = 4000;
+
+// middleware
+app.use(express.json()) // For parsing requests from frontend to backend
+app.use(cors()) // Enables us to access the backend from any frontend
+
+
+// The get method
+app.get("/",(req,res)=>{
+    
+}) // HTTP method used to request data from the server
