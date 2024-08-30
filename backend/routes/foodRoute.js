b05182b785e142f44e6312e5e94e7cf79e51ee01
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-30 15:51:08 +0300

    Initial

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,3 +1,13 @@
 import express from "express"
 import { addFood  } from "../controllers/foodController.js"
-import multer from "multer"
+import multer from "multer" // To create the image storage system
+
+const foodRouter = express.Router(); // To use to create the different methods ie; GET,POST,DELETE...
+
+
+
+
+
+
+
+export default foodRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 
 
@@ -8,13 +9,29 @@ import { connectDB } from "./config/db.js";
 const app = express();
 const port = 4000; // port to be used
 
+
+
+
 // middleware
 app.use(express.json()) // For parsing requests from frontend to backend
 app.use(cors()) // Enables us to access the backend from any frontend
 
 
+
+
+
 // DB CONNECTION
 connectDB();
+
+
+
+
+
+// api endponits
+app.use("/api/food",foodRouter)
+
+
+
 
 // The get method
 app.get("/",(req,res)=>{
@@ -22,6 +39,9 @@ app.get("/",(req,res)=>{
     res.send("API working") // Response sent when ever the endpoint is opened
 
 }) // The get method is an HTTP method used to request data from the server
+
+
+
 
 
 app.listen(port,()=>{
