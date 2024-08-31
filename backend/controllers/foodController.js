47e94fbc07060bc0a36348de5c1390cfff3edbdd
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-08-31 21:53:11 +0300

    Initial

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,8 +3,20 @@ import fs from "fs"
 
 
 //add food item
+// To use to add and store product data in database
 const addFood = async (req,res) => {
-    
+
+    let image_filename = `${req.file.filename}`;
+
+    // creating a new food using the food model
+    const newFood = new foodModel({
+        
+        name: req.body.name, // food name is got from the name field
+        description: req.body.description, // description is got from the description field
+        price: req.body.price, // price is got from the price field
+        category:req.body.category, // category is got from the category field
+        image: image_filename // image filename is got from the image field
+    })
 }
 
 export {addFood}
