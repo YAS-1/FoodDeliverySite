465866b49f84e87665654b238584b7f7bbf53fdd
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-09-02 20:37:07 +0300

    Initial

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -7,17 +7,21 @@ const foodRouter = express.Router(); // To use to create the different methods i
 
 
 // Image storage engine
+// creating a storage configuration object "storage"
 const storage = multer.diskStorage({
     destination:"uploads", // The destination where the image will be stored "the uploads folder"
     filename:(req,file,cb) => {
         return cb(null,`${Date.now()} ${file.originalname}`) // The time stamp when the image is added is set as the file original name to make it unique
     }
 })
+
+// Creating the "upload" middleware to handle the file upload process and saving
 const upload = multer({storage:storage}) // To use the storage engine in the router
 
 
 
 // foodRouter
+// foodRouter.post sends a request to the server through the end point "/add" to complete two tasks ie: 1 to upload the file entered within the image form field to the specified destination. 2 To execute the addFood function
 foodRouter.post("/add",upload.single("image"),addFood) // Used to send data on the server, when processed we get a response
 
 
