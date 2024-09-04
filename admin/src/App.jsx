d34f508bbd0adc594f16194cde13c1b168004f08
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-09-04 20:36:56 +0300

    Initial

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './pages/Sidebar/Sidebar'
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
+      <div className="app-content">
+        <Sidebar/>
+    
+      </div>
     </div>
   )
 }
