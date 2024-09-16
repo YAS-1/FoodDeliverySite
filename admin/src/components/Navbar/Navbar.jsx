1c417a2d402d27616c4be628658f1a7dec9ba22b
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-09-16 09:43:36 +0300

    Initial

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -6,9 +6,11 @@ import {assets} from '../../assets/assets'
 function Navbar() {
   return (
     <div className='navbar'>
-        <img className="logo" src={assets.logo_1} alt="" />
-        <img className='profile' src="" alt="" />
+      <img className="logo" src={assets.logo_1} alt="" />
+      <img className='profile' src={assets.profile_image_1} alt="" />
+      <hr />
     </div>
+    
   )
 }
 
